016b0e1016d64262ba223bebd9d1bf56e63c3daa
Chimpu Framework <developers@iwcnetwork.com>
2014-01-22 19:17:35 +0530

    Update db_class.php

--- protected/library/db_class.php
+++ protected/library/db_class.php
@@ -1,17 +1,6 @@
 <?php
 /**
- * smplPDO - PHP Mysql Database Helper Class
- *
- * smplPDO is a simple and light-weight class written to extend PHP Data Objects (PDO) 
- * with extremely useful features. Shorthand methods packed with it can reduce the 
- * amount of duplicate code and increase readability of your scripts as well as improve 
- * security and performance with automatically preparing & executing prepared statements.
- *
- * @package smplPDO
- * @version 1.1
- * @author Conrad Warhol
- * @link http://codecanyon.net/item/smplpdo-mysql-database-helper-class/3359730
- * @copyright 2012 Conrad Warhol
+ * PDO CLASS
  *
  */
 
